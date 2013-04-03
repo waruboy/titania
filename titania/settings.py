4119086fb698090109bac782c1fc24533a04e41a
Taufiq Muhammadi <waruboy@yahoo.com>
2013-04-03 15:44:02 +0700

    set static dir

--- titania/settings.py
+++ titania/settings.py
@@ -1,6 +1,8 @@
 # Django settings for titania project.
 import os
 
+PROJECT_DIR = os.path.join(os.path.dirname(__file__),'..')
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -62,7 +64,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
